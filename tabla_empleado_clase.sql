9d8a9d0a30866b9fb2566fc44db086f3d07371f4
Javi <jmerinocoll@gmail.com>
2018-03-06 18:55:27 +0100

    cambios

--- tabla_empleado_clase.sql
+++ tabla_empleado_clase.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `empleado_clase` (
-	`id_empleado` INT(11) NOT NULL,
-	`hora` TIME NOT NULL,
-	`dia_semana` DATETIME NOT NULL,
+	`id_empleado` INT(11),
+	`hora` TIME,
+	`dia_semana` INT(11),
 	PRIMARY KEY (`id_empleado`, `hora`, `dia_semana`),
 	CONSTRAINT `FK_empleado_clase_empleado` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 )
